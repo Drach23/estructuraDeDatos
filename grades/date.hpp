f075e05102fd75500e6cba0a676ab973eb6c1095
Drach23 <88101742+Drach23@users.noreply.github.com>
2023-06-08 21:09:56 -0600

    configuracion de constructores,getters,setters, operadores de comparacion, igualdad y funciones friend

--- grades/date.hpp
+++ grades/date.hpp
@@ -1,8 +1,46 @@
-//
-// Created by Richard on 08/06/2023.
-//
-
 #ifndef C_DATE_HPP
 #define C_DATE_HPP
 
+#include <iostream>
+#include <string>
+
+
+class Date{
+private:
+    int year;
+    int month;
+    int day;
+
+    int toInt() const;
+public:
+    Date(); //constructor
+    Date (const Date&); //constructor copia
+
+    //getters
+    int getYear() const;
+    int getMonth() const;
+    int getDay() const;
+
+    std::string toString() const;
+
+    //setters
+    void setYear(const int);
+    void setMonth(const int);
+    void setDay(const int);
+
+    //Operadores de comparacion entre objetos
+    bool operator == (const Date&) const;
+    bool operator != (const Date&) const;
+    bool operator <  (const Date&) const;
+    bool operator <= (const Date&) const;
+    bool operator >  (const Date&) const;
+    bool operator >= (const Date&) const;
+
+    //operador de igualdad para igualar objetos
+    Date& operator = (const Date&);
+
+    //funciones friend para poder asignar con cin e imprimir con cout.
+    friend std::ostream& operator << (std::ostream&,Date&);
+    friend std::istream& operator >> (std::istream&,Date&);
+};
 #endif //C_DATE_HPP
