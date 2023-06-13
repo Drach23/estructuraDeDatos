4c1ef7f26352dcc95e95664b5fca9b0fe61b2c72
Drach23 <88101742+Drach23@users.noreply.github.com>
2023-06-13 16:51:24 -0600

    Diseño de la clase menu para poner una interface y funcionalidades

--- grades/menu.hpp
+++ grades/menu.hpp
@@ -1,8 +1,22 @@
-//
-// Created by Richard on 08/06/2023.
-//
-
 #ifndef C_MENU_HPP
 #define C_MENU_HPP
+#include <iostream>
+#include <string>
 
+#include "student.hpp"
+#include "teacher.hpp"
+
+
+class Menu{
+protected:
+    Student students[10];
+    int numStudents;
+public:
+    Menu();
+    void start();
+    void addTeacher();
+    void addStudent();
+    void showStudents();
+    void showTeachers();
+};
 #endif //C_MENU_HPP
